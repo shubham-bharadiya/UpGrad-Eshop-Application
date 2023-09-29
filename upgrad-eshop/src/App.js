98dc7770f541653719b4aa4aa261cbd9e7b62cc4
shubham-bharadiya <shubhambharadiya@gmail.com>
2023-09-29 20:25:22 +0530

    Added toasts, basic signup file and styling, added some mui components related to signup component

--- upgrad-eshop/src/App.js
+++ upgrad-eshop/src/App.js
@@ -1,11 +1,31 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material";
+import { AuthContextProvider } from "../src/common/Auth/AuthContext";
+import SignUp from "../src/components/SignUp/SignUp";
 import './App.css';
 
+const appTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#3f51b5",
+    },
+    secondary: {
+      main: "#f44336",
+    },
+  },
+});
 function App() {
-  return (
-    <div>
-      {/* to be updated */}
-    </div>
-  );
+    return (
+      <AuthContextProvider>
+      <ThemeProvider theme={appTheme}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </AuthContextProvider>
+      )
 }
 
 export default App;
---
+++ upgrad-eshop/src/common/MuiComponents/TextField/SignUpTextMui.jsx
@@ -0,0 +1,20 @@
+import { TextField } from "@mui/material";
+import React from "react";
+
+const SignUpTextMui = (props) => {
+  return (
+    <TextField
+      required
+      variant="outlined"
+      type={props.type}
+      sx={{ mb: 3 }}
+      fullWidth
+      label={props.label}
+      onChange={props.onChange}
+      value={props.firstName}
+      error={props.firstNameError}
+    />
+  );
+};
+
+export default SignUpTextMui;
